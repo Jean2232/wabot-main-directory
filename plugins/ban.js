7bde3d2d01385eef80a06a8094bc9a2bf08f2fb0
Jean2232 <77308262+Jean2232@users.noreply.github.com>
2021-05-18 22:21:23 -0300

    Update ban.js

--- plugins/ban.js
+++ plugins/ban.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
-handler.help = ['ban', '-'].map(v => v + ' @user')
-handler.tags = ['admin']
-handler.command = /^(ban|\-)$/i
+handler.help = ['kick','-'].map(v => 'o' + v + ' @user')
+handler.tags = ['owner']
+handler.command = /^(okick|o\-)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
@@ -13,9 +13,8 @@ handler.group = true
 handler.private = false
 
 handler.admin = true
-handler.botAdmin = true
+handler.botAdmin = false
 
 handler.fail = null
-handler.limit = true
 
-module.exports = handler
+module.exports = 
