cb7f81e5ae023e6c82aee1739c48b0be0ed35aa5
Jean2232 <77308262+Jean2232@users.noreply.github.com>
2021-05-18 22:24:12 -0300

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, text }) => {
   let users = text.split`,`.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v.length > 20)
   conn.groupAdd(m.chat, users)
 }
-handler.help = ['add', '+'].map(v => 'o' + v + ' número')
-handler.tags = ['owner']
-handler.command = /^(oadd|o\+)$/i
+handler.help = ['add + número']
+handler.tags = ['admin']
+handler.command = /^(add|o\+)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
